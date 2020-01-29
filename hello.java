72eb0513495945194361ea4dc896e2293fa29bb2
Fazil Shaikh <fazil.shaikh@maine.edu>
2020-01-29 10:20:50 -0500

    Outro Message
    added the goodbye message

--- hello.java
+++ hello.java
@@ -4,6 +4,7 @@ public class hello {
     public static void main (String[] args) {
     	System.out.println ("Here ye : important announcement");
     	System.out.println ("Hello World!");
+    	System.out.println ("Thank you for your attention. Goodbye");
     }
 
 }
