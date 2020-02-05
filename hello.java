9e5cab949d97f8d6a5d483c7e677544e3c238234
Fazil Shaikh <fazil.shaikh@maine.edu>
2020-02-05 11:14:47 -0500

    Update hello.java
    added current date

--- hello.java
+++ hello.java
@@ -3,6 +3,13 @@ public class hello {
 
     public static void main (String[] args) {
     	System.out.println ("Here ye : important announcement");
+		/* This will display the date and time in the format of
+		 * 12/09/2017 24:12:35. See the complete program below
+		 */
+		DateFormat df = new SimpleDateFormat("dd/MM/yy HH:mm:ss");
+		Date dateobj = new Date();
+		System.out.println(df.format(dateobj));
+
     	System.out.println ("Hello World!");
     	System.out.println ("Hi Alice");
     	System.out.println ("Hi Bob");
